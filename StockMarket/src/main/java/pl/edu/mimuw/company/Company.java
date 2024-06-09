45cd1243fb0c29666ecb6821f95d4bee156eed3c
witek-formanski <witekformanski@gmail.com>
2024-06-09 18:13:10 +0200

    I hate forum

--- StockMarket/src/main/java/pl/edu/mimuw/company/Company.java
+++ StockMarket/src/main/java/pl/edu/mimuw/company/Company.java
@@ -8,8 +8,6 @@ import pl.edu.mimuw.order.Order;
 import pl.edu.mimuw.order.purchase.Purchase;
 import pl.edu.mimuw.order.sale.Sale;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.PriorityQueue;
 import java.util.Queue;
 
@@ -71,14 +69,17 @@ public class Company {
         );
     }
 
-    private void tryCreateTransaction() { //ToDo: binary orders
+    private void tryCreateTransaction() {
+        if (purchases.peek() instanceof BinaryOrder || sales.peek() instanceof BinaryOrder) {
+            tryCreateBinaryTransaction();
+        } else {
+            createTransaction();
+        }
+    }
+
+    private void createTransaction() {
         Purchase purchase = purchases.peek();
         Sale sale = sales.peek();
-
-        if (purchase instanceof BinaryOrder || sale instanceof BinaryOrder) {
-            tryCreateBinaryTransaction();
-            return;
-        }
 
         int sharesCount = Math.min(purchase.getSharesCount(), sale.getSharesCount());
         int price = discussPrice(purchase, sale);
@@ -92,39 +93,94 @@ public class Company {
             sales.poll();
         }
 
-        Logger.log("Transaction was completed.");
+        setLastPriceOfShare(price);
+        Logger.log("Transaction was completed. Transferred " + sharesCount + " shares of " + name + " for " + price + " each.");
     }
 
     private void tryCreateBinaryTransaction() {
-        BinaryTransactionsResolver resolver = new BinaryTransactionsResolver();
-        resolver.tryResolve();
+//        BinaryTransactionsResolver resolver = new BinaryTransactionsResolver();
+//        resolver.tryResolve();
     }
 
     private int discussPrice(Purchase purchase, Sale sale) {
         return (purchase.getOrderNumber() < sale.getOrderNumber()) ? purchase.getPriceLimit() : sale.getPriceLimit();
     }
 
-    private class BinaryTransactionsResolver {
-        List<OrderPair<Purchase>> purchaseList;
-        List<OrderPair<Sale>> saleList;
-
-        private BinaryTransactionsResolver() {
-            purchaseList = new ArrayList<>();
-            saleList = new ArrayList<>();
-        }
-
-        public void tryResolve() {
-
-        }
-    }
-
-    private class OrderPair<T extends Order> {
-        private T order;
-        private boolean isPossible;
-
-        private OrderPair(T order, boolean isPossible) {
-            this.order = order;
-            this.isPossible = isPossible;
-        }
-    }
+//    private class BinaryTransactionsResolver {
+//        private void tryResolve() {
+//            Purchase firstPurchase = purchases.peek();
+//            Sale firstSale = sales.peek();
+//
+//            if (!(firstPurchase instanceof BinaryOrder) && !(firstSale instanceof BinaryOrder)) {
+//                throw new IllegalStateException("Tried to resolve binary orders, but neither of them is binary.");
+//            }
+//
+//            List<OrderPair<Purchase>> purchaseList;
+//            List<OrderPair<Sale>> saleList;
+//            if (firstPurchase instanceof BinaryOrder && firstSale instanceof BinaryOrder) {
+//
+//            } else if (firstPurchase instanceof BinaryOrder) {
+//                int balance = firstSale.getSharesCount() - firstPurchase.getSharesCount();
+//                if (balance >= 0) {
+//                    // to się da
+//                }
+//
+//                purchaseList = new ArrayList<>();
+//                saleList = new ArrayList<>();
+//                purchaseList.add(new OrderPair<>(firstPurchase, true));
+//                saleList.add(new OrderPair<>(firstSale, true));
+//
+//
+//                tryResolve(purchaseList, saleList, balance);
+//            } else {
+//                int balance = firstSale.getSharesCount() - firstPurchase.getSharesCount();
+//                if (balance <= 0) {
+//                    // to się da
+//                }
+//            }
+//        }
+//
+//        private boolean tryResolve(List<OrderPair<Purchase>> purchaseList, List<OrderPair<Sale>> saleList, int balance) {
+//            if (balance == 0) {
+//                return true;
+//            }
+//            if (balance > 0) { // more to sell than to buy
+//
+//            }
+//        }
+//
+//        private void createBinaryTransaction(List<OrderPair<Purchase>> purchaseList, List<OrderPair<Sale>> saleList) {
+//            while (!purchaseList.isEmpty() && !purchaseList.isEmpty()) { // both non-empty
+//                if (purchaseList.getFirst().order != purchases.peek() || saleList.getFirst().order != sales.peek()) {
+//                    throw new IllegalStateException("Invalid binary purchase or sale list.");
+//                }
+//                if (!purchaseList.getFirst().isPossible) {
+//                    purchaseList.removeFirst();
+//                    purchases.poll();
+//                    continue;
+//                }
+//                if (!saleList.getFirst().isPossible) {
+//                    saleList.removeFirst();
+//                    sales.poll();
+//                    continue;
+//                }
+//
+//            }
+//        }
+//    }
+//
+//    private class OrderPair<T extends Order> {
+//        private T order;
+//        private boolean isPossible;
+//
+//        private OrderPair(T order, boolean isPossible) {
+//            this.order = order;
+//            this.isPossible = isPossible;
+//        }
+//
+//        private OrderPair(T order) {
+//            this.order = order;
+//            isPossible = !(order instanceof BinaryOrder);
+//        }
+//    }
 }
